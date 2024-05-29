d5b016f7e331005faa8fa7561c624cf01f75d0dc
unilight <vincenthuang316@gmail.com>
2024-05-29 12:54:48 +0900

    nisqa: runnable

---
+++ egs/nisqa/local/data_prep.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2024 Wen-Chin Huang
+#  MIT License (https://opensource.org/licenses/MIT)
+
+"""Data preparation for NISQA."""
+
+import argparse
+import csv
+import librosa
+import logging
+import os
+import soundfile as sf
+import sys
+from tqdm import tqdm
+
+from sheet.utils import read_csv
+
+def main():
+    """Run training process."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--original-path",
+        required=True,
+        type=str,
+        help=("original csv file path."),
+    )
+    parser.add_argument(
+        "--wavdir",
+        required=True,
+        type=str,
+        help=("directory of the waveform files. This is needed because wav paths in the metadata files, the wav dir is not contained."),
+    )
+    parser.add_argument(
+        "--out",
+        required=True,
+        type=str,
+        help=("output csv file path."),
+    )
+    parser.add_argument(
+        "--resample",
+        action='store_true',
+        help=("whether to perform resampling or not."),
+    )
+    parser.add_argument(
+        "--target-sampling-rate",
+        type=int,
+        help=("target sampling rate."),
+    )
+    parser.add_argument(
+        "--resample-backend",
+        type=str,
+        default="librosa",
+        choices=["librosa"],
+        help=("resample backend."),
+    )
+    parser.add_argument(
+        "--target-wavdir",
+        type=str,
+        help=("directory of the resampled waveform files."),
+    )
+    args = parser.parse_args()
+
+    # set logger
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stdout,
+        format="%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s",
+    )
+
+    # make resampled dir and dynamic import
+    if args.resample:
+        os.makedirs(args.target_wavdir, exist_ok=True)
+        if args.resample_backend == "librosa":
+            import librosa
+
+    # read csv
+    logging.info("Reading original csv file.")
+    filelist, _ = read_csv(args.original_path, dict_reader=True)
+
+    # prepare. header:
+    # db,con,file,con_description,filename_deg,filename_ref,source,lang,votes,mos,noi,col,dis,loud,noi_std,col_std,dis_std,loud_std,mos_std,filepath_deg,filepath_ref,filter,timeclipping,wbgn,p50mnru,bgn,clipping,arb_filter,asl_in,asl_out,codec1,codec2,codec3,plcMode1,plcMode2,plcMode3,wbgn_snr,bgn_snr,tc_fer,tc_nburst,cl_th,bp_low,bp_high,p50_q,bMode1,bMode2,bMode3,FER1,FER2,FER3,asl_in_level,asl_out_level
+    logging.info("Preparing metadata.")
+    metadata = []
+    for line in tqdm(filelist):
+        if len(line) == 0: continue
+        system_id = line["con"]
+        wav_path = line["filename_deg"]
+        complete_wav_path = os.path.join(args.wavdir, wav_path)
+        sample_id = os.path.splitext(wav_path)[0]
+        score = float(line["mos"])
+
+        # if resample and resample is necessary
+        if args.resample and librosa.get_samplerate(complete_wav_path) != args.target_sampling_rate:
+            resampled_wav_path = os.path.join(args.target_wavdir, wav_path)
+            # resample and write if not exist yet
+            if not os.path.isfile(resampled_wav_path):
+                if args.resample_backend == "librosa":
+                    resampled_wav, _ = librosa.load(complete_wav_path, sr=args.target_sampling_rate)
+                sf.write(resampled_wav_path, resampled_wav, samplerate=args.target_sampling_rate)
+            complete_wav_path = resampled_wav_path
+
+        item = {
+            "wav_path": complete_wav_path,
+            "score": score,
+            "system_id": system_id,
+            "sample_id": sample_id,
+        }
+        metadata.append(item)
+
+    # write csv
+    logging.info("Writing output csv file.")
+    fieldnames = ["wav_path", "score", "system_id", "sample_id"]
+    with open(args.out, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for line in metadata:
+            writer.writerow(line)
+
+if __name__ == "__main__":
+    main()
---
+++ egs/nisqa/run.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 Wen-Chin Huang (Nagoya University)
+#  MIT License (https://opensource.org/licenses/MIT)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+# basic settings
+stage=-1       # stage to start
+stop_stage=100 # stage to stop
+verbose=1      # verbosity level (lower is less info)
+n_gpus=1       # number of gpus in training
+n_jobs=16      # number of parallel jobs in feature extraction
+seed=1337
+
+conf=conf/ssl-mos-wav2vec2.yaml
+
+# dataset configuration
+db_root=/data/group1/z44476r/Corpora/nisqa/NISQA_Corpus
+bvcc_db_root=/data/group1/z44476r/Corpora/BVCC/main/DATA
+target_sampling_rate=16000
+
+# training related setting
+tag=""     # tag for directory to save model
+resume=""  # checkpoint path to resume training
+           # (e.g. <path>/<to>/checkpoint-10000steps.pkl)
+           
+# decoding related setting
+# test_sets="LIVETALK FOR P501"
+test_sets="bvcc_dev bvcc_test LIVETALK FOR P501"
+checkpoint=""               # checkpoint path to be used for decoding
+                            # if not provided, the latest one will be used
+                            # (e.g. <path>/<to>/checkpoint-400000steps.pkl)
+model_averaging="False"
+use_stacking="False"
+meta_model_checkpoint=""
+                                       
+# shellcheck disable=SC1091
+. utils/parse_options.sh || exit 1;
+
+set -euo pipefail
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    echo "stage -1: Data and Pretrained Model Download"
+
+    # Just provide instructions?
+
+fi
+
+mkdir -p "data"
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    echo "stage 0: Data preparation"
+
+    # parse original csv file to an unified format
+    local/data_prep.py \
+        --original-path "${db_root}/NISQA_TRAIN_SIM/NISQA_TRAIN_SIM_file.csv" --wavdir "${db_root}/NISQA_TRAIN_SIM/deg" --out "data/train_sim.csv" \
+        --resample --target-sampling-rate "${target_sampling_rate}" --target-wavdir "${db_root}/NISQA_TRAIN_SIM/deg_${target_sampling_rate}" 
+    local/data_prep.py \
+        --original-path "${db_root}/NISQA_TRAIN_LIVE/NISQA_TRAIN_LIVE_file.csv" --wavdir "${db_root}/NISQA_TRAIN_LIVE/deg" --out "data/train_live.csv" \
+        --resample --target-sampling-rate "${target_sampling_rate}" --target-wavdir "${db_root}/NISQA_TRAIN_LIVE/deg_${target_sampling_rate}" 
+    local/data_prep.py \
+        --original-path "${db_root}/NISQA_VAL_SIM/NISQA_VAL_SIM_file.csv" --wavdir "${db_root}/NISQA_VAL_SIM/deg" --out "data/dev_sim.csv" \
+        --resample --target-sampling-rate "${target_sampling_rate}" --target-wavdir "${db_root}/NISQA_VAL_SIM/deg_${target_sampling_rate}"
+    local/data_prep.py \
+        --original-path "${db_root}/NISQA_VAL_LIVE/NISQA_VAL_LIVE_file.csv" --wavdir "${db_root}/NISQA_VAL_LIVE/deg" --out "data/dev_live.csv" \
+        --resample --target-sampling-rate "${target_sampling_rate}" --target-wavdir "${db_root}/NISQA_VAL_LIVE/deg_${target_sampling_rate}"
+    # for test_set in ${test_sets}; do
+    for test_set in LIVETALK FOR P501; do
+        local/data_prep.py \
+            --original-path "${db_root}/NISQA_TEST_${test_set}/NISQA_TEST_${test_set}_file.csv" --wavdir "${db_root}/NISQA_TEST_${test_set}/deg" --out "data/${test_set}.csv" \
+            --resample --target-sampling-rate "${target_sampling_rate}" --target-wavdir "${db_root}/NISQA_TEST_${test_set}/deg_${target_sampling_rate}"
+    done
+
+    # combine sim and live
+    utils/combine_datasets.py --original-paths "data/train_sim.csv" "data/train_live.csv" --out "data/train.csv"
+    utils/combine_datasets.py --original-paths "data/dev_sim.csv" "data/dev_live.csv" --out "data/dev.csv"
+
+    # BVCC dev & test sets
+    ../bvcc/local/data_prep.py \
+        --original-path "${bvcc_db_root}/sets/DEVSET" --wavdir "${bvcc_db_root}/wav" --out "data/bvcc_dev.csv"
+    ../bvcc/local/data_prep.py \
+        --original-path "${bvcc_db_root}/sets/TESTSET" --wavdir "${bvcc_db_root}/wav" --out "data/bvcc_test.csv"
+
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo "stage 1: Feature extraction"
+    echo "No feature extraction needed currently"
+fi
+
+if [ -z ${tag} ]; then
+    expname="$(basename ${conf%.*})-${seed}"
+else
+    expname="${tag}-${seed}"
+fi
+expdir=exp/${expname}
+if [ "${stage}" -le 2 ] && [ "${stop_stage}" -ge 2 ]; then
+    echo "Stage 2: Network training"
+    [ ! -e "${expdir}" ] && mkdir -p "${expdir}"
+    if [ "${n_gpus}" -gt 1 ]; then
+        echo "Not Implemented yet."
+        # train="python -m seq2seq_vc.distributed.launch --nproc_per_node ${n_gpus} -c parallel-wavegan-train"
+    else
+        train="train.py"
+    fi
+    echo "Training start. See the progress via ${expdir}/train.log."
+    ${cuda_cmd} --gpu "${n_gpus}" "${expdir}/train.log" \
+        ${train} \
+            --config "${conf}" \
+            --train-csv-path "data/train.csv" \
+            --dev-csv-path "data/dev.csv" \
+            --outdir "${expdir}" \
+            --resume "${resume}" \
+            --verbose "${verbose}" \
+            --seed "${seed}"
+    echo "Successfully finished training."
+fi
+
+if [ "${stage}" -le 3 ] && [ "${stop_stage}" -ge 3 ]; then
+    echo "Stage 3: Inference"
+    # shellcheck disable=SC2012
+
+    if [ "${use_stacking}" = "True" ]; then
+        [ -z "${meta_model_checkpoint}" ] && meta_model_checkpoint="${expdir}/meta_model.pkl"
+        outdir="${expdir}/results/stacking-model"
+    elif [ "${model_averaging}" = "True" ]; then
+        outdir="${expdir}/results/model-averaging"
+    else
+        [ -z "${checkpoint}" ] && checkpoint="${expdir}/checkpoint-best.pkl"
+        outdir="${expdir}/results/$(basename "${checkpoint}" .pkl)"
+    fi
+
+    for name in ${test_sets}; do
+        [ ! -e "${outdir}/${name}" ] && mkdir -p "${outdir}/${name}"
+        [ "${n_gpus}" -gt 1 ] && n_gpus=1
+        echo "Inference start. See the progress via ${outdir}/${name}/inference.log."
+        ${cuda_cmd} --gpu "${n_gpus}" "${outdir}/${name}/inference.log" \
+            inference.py \
+                --config "${expdir}/config.yml" \
+                --csv-path "data/${name}.csv" \
+                --checkpoint "${checkpoint}" \
+                --outdir "${outdir}/${name}" \
+                --model-averaging "${model_averaging}" \
+                --use-stacking "${use_stacking}" \
+                --meta-model-checkpoint "${meta_model_checkpoint}" \
+                --verbose "${verbose}"
+        echo "Successfully finished inference of ${name} set."
+        grep "UTT" "${outdir}/${name}/inference.log"
+    done
+    echo "Successfully finished inference."
+fi
