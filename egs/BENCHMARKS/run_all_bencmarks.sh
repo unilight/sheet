db99c9bdf2f6c6c44cf51cbe95444601f39baf94
unilight <vincenthuang316@gmail.com>
2025-09-03 18:21:30 +0900

    refactor: run_all_benchmarks.sh

--- egs/BENCHMARKS/run_all_bencmarks.sh
+++ egs/BENCHMARKS/run_all_bencmarks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2024 Wen-Chin Huang (Nagoya University)
+# Copyright 2025 Wen-Chin Huang (Nagoya University)
 #  MIT License (https://opensource.org/licenses/MIT)
 
 conf=
@@ -24,10 +24,20 @@ if [ ! -z ${tag} ]; then
     _opts+="--tag ${tag} "
 fi
 
+# synthetic speech
 utils/BENCHMARKS/run_bvcc_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
-utils/BENCHMARKS/run_bc19_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
 utils/BENCHMARKS/run_somos_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_bc19_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_bc23_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_svcc23_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
 utils/BENCHMARKS/run_singmos_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_brspeechmos_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_hablamos_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_ttsds2_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+
+# distorted speech
 utils/BENCHMARKS/run_nisqa_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
 utils/BENCHMARKS/run_tmhint_qi_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
-utils/BENCHMARKS/run_vmc23_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_tmhint_qi_s_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_tcd_voip_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
+utils/BENCHMARKS/run_vmc24_track3_test.sh --conf ${conf} --seed ${seed} --checkpoint ${checkpoint} ${_opts}
